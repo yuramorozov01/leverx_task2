a1b133f0b4b7ff0b5296815e1dfd27230901456d
yuramorozov01 <yura_morozov01@mail.ru>
2021-12-05 21:26:10 +0300

    feat: extend Version class for semantic comparison

---
+++ task_2.py
@@ -0,0 +1,44 @@
+from packaging.version import parse
+
+
+class Version:
+    def __init__(self, version):
+        self.version = parse(version)
+
+    def __lt__(self, other):
+        return self.version < other.version
+
+    def __le__(self, other):
+        return self.version <= other.version
+
+    def __eq__(self, other):
+        return self.version == other.version
+
+    def __ne__(self, other):
+        return self.version != other.version
+
+    def __gt__(self, other):
+        return self.version > other.version
+
+    def __ge__(self, other):
+        return self.version >= other.version
+
+
+def main():
+    to_test = [
+        ("1.0.0", "2.0.0"),
+        ("1.0.0", "1.42.0"),
+        ("1.2.0", "1.2.42"),
+        ("1.1.0-alpha", "1.2.0-alpha.1"),
+        ("1.0.1b", "1.0.10-alpha.beta"),
+        ("1.0.0-rc.1", "1.0.0"),
+    ]
+
+    for version_1, version_2 in to_test:
+        assert Version(version_1) < Version(version_2), "le failed"
+        assert Version(version_2) > Version(version_1), "ge failed"
+        assert Version(version_2) != Version(version_1), "neq failed"
+
+
+if __name__ == "__main__":
+    main()
